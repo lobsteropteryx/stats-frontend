33ea4e0b3d94b442fd4e57b6ca3330b5578bdcc4
ifirkin <ian.firkin@homeinsteadinc.com>
2017-11-23 00:46:45 -0500

    Adds better bar chart

--- trello.js
+++ trello.js
@@ -1,51 +1,45 @@
 const display = (cards) => {
-    const displayData = rollupByMonth(cards);
-
-    buildChart(displayData);
-
-    const pre = document.createElement('pre');
-    pre.innerText = JSON.stringify(displayData, null, 4);
-    document.body.appendChild(pre);
+    buildChart(rollupByMonth(cards));
 };
 
 const buildChart = (data) => {
-    const width = 420;
-    const barHeight = 20;
+    const svg = d3.select("svg"),
+        margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
 
-    const x = d3.scaleLinear()
-        .range([0, width]);
+    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+        y = d3.scaleLinear().rangeRound([height, 0]);
 
-    const chart = d3.select(".chart")
-        .attr("width", width);
+    const g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    x.domain([0, d3.max(data, function (d) {
-        return d.value;
-    })]);
+    x.domain(data.map(d => d.key));
+    y.domain([0, d3.max(data, d => d.value)]);
 
-    chart.attr("height", barHeight * data.length);
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-    const bar = chart.selectAll("g")
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Count");
+
+    g.selectAll(".bar")
         .data(data)
-        .enter().append("g")
-        .attr("transform", function (d, i) {
-            return "translate(0," + i * barHeight + ")";
-        });
-
-    bar.append("rect")
-        .attr("width", function (d) {
-            return x(d.value);
-        })
-        .attr("height", barHeight - 1);
-
-    bar.append("text")
-        .attr("x", function (d) {
-            return x(d.value) - 3;
-        })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function (d) {
-            return d.value;
-        });
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.key))
+        .attr("y", d => y(d.value))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.value));
 };
 
 const rollupByMonth = (cards) => {
