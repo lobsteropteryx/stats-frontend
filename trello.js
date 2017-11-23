7c7f9efa37348546257f40d9ccf0732a72ab6105
ifirkin <ian.firkin@homeinsteadinc.com>
2017-11-23 00:22:53 -0500

    Adds basic bar chart

--- trello.js
+++ trello.js
@@ -1,15 +1,58 @@
 const display = (cards) => {
-    var displayData = rollupByMonth(getBugs(cards));
-    var pre = document.createElement('pre');
+    const displayData = rollupByMonth(cards);
+
+    buildChart(displayData);
+
+    const pre = document.createElement('pre');
     pre.innerText = JSON.stringify(displayData, null, 4);
     document.body.appendChild(pre);
+};
+
+const buildChart = (data) => {
+    const width = 420;
+    const barHeight = 20;
+
+    const x = d3.scaleLinear()
+        .range([0, width]);
+
+    const chart = d3.select(".chart")
+        .attr("width", width);
+
+    x.domain([0, d3.max(data, function (d) {
+        return d.value;
+    })]);
+
+    chart.attr("height", barHeight * data.length);
+
+    const bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function (d, i) {
+            return "translate(0," + i * barHeight + ")";
+        });
+
+    bar.append("rect")
+        .attr("width", function (d) {
+            return x(d.value);
+        })
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function (d) {
+            return x(d.value) - 3;
+        })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function (d) {
+            return d.value;
+        });
 };
 
 const rollupByMonth = (cards) => {
     return d3.nest()
         .key(d => d.date.format('YYYY-MM'))
         .rollup(v => v.length)
-        .entries(cards);
+        .entries(getBugs(cards));
 };
 
 const getBugs = (cards) => {
