1ff6f06b43c7e901b45738e22101a9d9a7762fcd
ifirkin <ian.firkin@homeinsteadinc.com>
2018-11-15 09:33:26 -0500

    Adds action to request boards

--- app/Trello/actions.js
+++ app/Trello/actions.js
@@ -1,10 +1,22 @@
-export const GET_BOARDS = "GET_BOARDS";
-export const SELECT_BOARD = "SELECT_BOARD";
+import Trello from "trello";
 
+
+export const GET_BOARDS = "GET_BOARDS";
 export function getBoards(boards) {
-    return { type: GET_BOARDS, boards}
+    return {type: GET_BOARDS, boards}
 }
 
+export const SELECT_BOARD = "SELECT_BOARD";
 export function selectBoard(board) {
-    return { type: SELECT_BOARD, board}
+    return {type: SELECT_BOARD, board}
+}
+
+export const FETCH_BOARDS = "FETCH_BOARDS";
+export function fetchBoards(apiKey, token) {
+    return (dispatch) => {
+        const trello = new Trello(apiKey, token);
+
+        return trello.getBoards('me')
+            .then(boards => dispatch(getBoards(boards)));
+    }
 }
--- test/Trello/actions.test.js
+++ test/Trello/actions.test.js
@@ -1,8 +1,13 @@
+import nock from 'nock';
+import { mockStore } from '../mockStore';
+
 import {
     getBoards,
     selectBoard,
+    fetchBoards,
     GET_BOARDS,
-    SELECT_BOARD
+    SELECT_BOARD,
+    FETCH_BOARDS
 } from '../../app/Trello/actions';
 
 describe("Trello Boards", () => {
@@ -18,5 +23,20 @@ describe("Trello Boards", () => {
         expect(selectBoard(board)).toEqual({
             type: SELECT_BOARD, board
         });
+    });
+
+    it("requests boards", async () => {
+        const boards = [{id: 1, name: 'my board'}];
+
+        const store = mockStore();
+
+        nock('https://api.trello.com')
+            .get('/1/members/me/boards')
+            .query(true)
+            .reply(200, boards);
+
+        await store.dispatch(fetchBoards('apiKey', 'token'));
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({type: GET_BOARDS, boards});
     });
 });
