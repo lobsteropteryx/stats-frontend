6aca4413b0550a839c5d1d146b54ca5af07efa9f
ifirkin <ian.firkin@hidigital.solutions>
2020-08-22 10:58:38 -0400

    Handles converting multiple actions to chart data

--- app/ScatterPlot/selectors.js
+++ app/ScatterPlot/selectors.js
@@ -1,8 +1,18 @@
 import { createSelector } from "reselect";
 
+const getActions = state => state.actions;
+
 export const getPlotData = createSelector(
-    [],
-    (durations) => {
-        return [];
+    [getActions],
+    (actions) => {
+        return actions.map( (action) => {
+            return {
+                id: action.id,
+                name: action.name,
+                url: `https://trello.com/c/${action.id}`,
+                x: action.completionDate.format("YYYY-MM-DD"),
+                y: parseFloat(action.duration.asDays(), 1).toPrecision(1)
+            };
+        });
     }
-)
+);
--- test/ScatterPlot/selectors.test.js
+++ test/ScatterPlot/selectors.test.js
@@ -1,10 +1,37 @@
+import moment from "moment"; 
 import { getPlotData } from "../../app/ScatterPlot/selectors";
 
 describe("Transforming chart data", () => {
     it("Returns an empty array when there are no actions", () => {
         const expected = [];
-        const durations = [];
-        const actual = getPlotData(durations);
+
+        const state = { actions: [] };
+
+        const actual = getPlotData(state);
+
+        expect(actual).toEqual(expected);
+    });
+    
+    it("Returns data elements from actions", () => {
+        const expected = [{
+            id: 1,
+            name: "test",
+            url: "https://trello.com/c/1",
+            x: "2020-01-01",
+            y: "1"
+        }];
+
+        const state = { 
+            actions: [{
+                id: 1,
+                name: "test",
+                completionDate: moment("2020-01-01"),
+                duration: moment.duration(1, "days")
+            }]
+        };
+
+        const actual = getPlotData(state);
+
         expect(actual).toEqual(expected);
     });
 });
