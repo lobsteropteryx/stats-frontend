e0894619be714271458943a8b1ed8a2c92f2f8cb
ifirkin <ian.firkin@hidigital.solutions>
2020-10-30 17:14:33 -0400

    Adds test for filtering with non-null start and end dates

--- test/filterActionByDate.test.js
+++ test/filterActionByDate.test.js
@@ -1,7 +1,7 @@
 import filterActionByDate from "../app/filterActionByDate";
 
 describe('Filtering actions', () => {
-    it('Returns true if startDate is null', () => {
+    it('Returns true if startDate is null and end date is less than completion date', () => {
         const action = {
             completionDate: new Date(1, 1, 2020)
         };
@@ -11,7 +11,17 @@ describe('Filtering actions', () => {
         expect(filterActionByDate(action, startDate, endDate)).toBe(true);
     });
 
-    it('Returns true if endDate is null', () => {
+    it('Returns true if startDate is greater than completion date and end date is less than completion date', () => {
+        const action = {
+            completionDate: new Date(1, 2, 2020)
+        };
+        const startDate = new Date(1, 1, 2020);
+        const endDate = new Date(1, 3, 2020);
+
+        expect(filterActionByDate(action, startDate, endDate)).toBe(true);
+    });
+
+    it('Returns true if endDate is null and start date is less than completion date', () => {
         const action = {
             completionDate: new Date(1, 2, 2020)
         };
