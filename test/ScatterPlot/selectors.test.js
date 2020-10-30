44ae71b3c39224bb06f0f9b09b9af076ddffbaa4
ifirkin <ian.firkin@hidigital.solutions>
2020-10-30 16:48:16 -0400

    Adds test to filter incomplete actions for scatter plot

--- test/ScatterPlot/selectors.test.js
+++ test/ScatterPlot/selectors.test.js
@@ -38,7 +38,47 @@ describe("Transforming chart data", () => {
                 actions: [{
                     id: 1,
                     name: "test",
+                    isComplete: true,
                     completionDate: moment("2020-01-01"),
+                    duration: moment.duration(1, "days")
+                }]
+            }
+        };
+
+        const actual = getPlotData(state);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it("Filters incomplete actions", () => {
+        const expected = [{
+            id: "Cards Completed",
+            data: [{
+                id: 2,
+                name: "test2",
+                url: "https://trello.com/c/2",
+                x: "2020-01-02",
+                y: "1"
+            }]
+        }];
+
+        const state = { 
+            date: {
+                startDate: moment("2020-01-02"),
+                endDate: moment("2020-01-02")
+            },
+            filter: {
+                actions: [{
+                    id: 1,
+                    name: "test",
+                    isComplete: false,
+                    completionDate: null,
+                    duration: null 
+                }, {
+                    id: 2,
+                    name: "test2",
+                    isComplete: true,
+                    completionDate: moment("2020-01-02"),
                     duration: moment.duration(1, "days")
                 }]
             }
@@ -70,16 +110,19 @@ describe("Transforming chart data", () => {
                 actions: [{
                     id: 1,
                     name: "test",
+                    isComplete: true,
                     completionDate: moment("2020-01-01"),
                     duration: moment.duration(1, "days")
                 }, {
                     id: 2,
                     name: "test2",
+                    isComplete: true,
                     completionDate: moment("2020-01-02"),
                     duration: moment.duration(1, "days")
                 }, {
                     id: 3,
                     name: "test3",
+                    isComplete: true,
                     completionDate: moment("2020-01-03"),
                     duration: moment.duration(1, "days")
                 }]
