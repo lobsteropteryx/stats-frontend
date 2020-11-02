31358c12003fcd6de6de745bb4138b5ce7aa2ad9
ifirkin <ian.firkin@hidigital.solutions>
2020-11-02 14:35:52 -0500

    Adds selector for wip items

--- app/WipPlot/selectors.js
+++ app/WipPlot/selectors.js
@@ -1,11 +1,20 @@
 import { createSelector } from "reselect";
+import { groupBy } from 'lodash';
 
 const getActions = state => state.filter.actions;
 const getStartDate = state => state.date.startDate;
 const getEndDate = state => state.date.endDate;
 
+const monthName = item => item.startDate.format('MMMM');
+
 const bucketActionsByDuration = (actions, startDate, endDate) => {
-    return [];
+    return Object.entries(groupBy(actions, monthName))
+        .map( item => {
+            return {
+                x: item[0],
+                y: item[1].length
+            };
+        });
 };
 
 export const getPlotData = createSelector(
--- test/WipPlot/selectors.test.js
+++ test/WipPlot/selectors.test.js
@@ -19,4 +19,34 @@ describe("Transforming WIP data", () => {
 
         expect(actual).toEqual(expected);
     });
+
+    describe("Incomplete actions", () => {
+        it("First day of the month", () => {
+            const expected = [{
+                id: "WIP",
+                data: [{
+                    x: "January",
+                    y: 1
+                }]
+            }];
+
+            const state = {
+                date: {}, 
+                filter: { 
+                    actions: [{
+                        id: 1,
+                        name: "test",
+                        startDate: moment("2020-01-01"),
+                        isComplete: false,
+                        completionDate: null,
+                        duration: null
+                    }]
+                } 
+            };
+
+            const actual = getPlotData(state);
+
+            expect(actual).toEqual(expected);
+        });
+    });
 });
