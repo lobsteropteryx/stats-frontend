b10dfc4c090a68875d4f9124f1d3d3e03427682c
ifirkin <ian.firkin@hidigital.solutions>
2020-11-02 14:37:09 -0500

    Tests wip for incomplete items on last day of month

--- test/WipPlot/selectors.test.js
+++ test/WipPlot/selectors.test.js
@@ -48,5 +48,33 @@ describe("Transforming WIP data", () => {
 
             expect(actual).toEqual(expected);
         });
+
+        it("Last day of the month", () => {
+            const expected = [{
+                id: "WIP",
+                data: [{
+                    x: "January",
+                    y: 1
+                }]
+            }];
+
+            const state = {
+                date: {}, 
+                filter: { 
+                    actions: [{
+                        id: 1,
+                        name: "test",
+                        startDate: moment("2020-01-31"),
+                        isComplete: false,
+                        completionDate: null,
+                        duration: null
+                    }]
+                } 
+            };
+
+            const actual = getPlotData(state);
+
+            expect(actual).toEqual(expected);
+        });
     });
 });
