88347ac31a3890b613d6fe34106c0f8f8e931a9e
ifirkin <ian.firkin@hidigital.solutions>
2021-03-01 12:32:46 -0500

    Sorts CSV data by card id and date

--- app/Filter/Query/csvExporter.ts
+++ app/Filter/Query/csvExporter.ts
@@ -1,8 +1,11 @@
 import jsonexport from "jsonexport";
 import { Card } from "../../card";
 
-export async function getCsvData(cards, boardName) {
-    const content = await convertToCsv(cards.map(transformDates));
+export async function getCsvData(cards: Card[], boardName: string) {
+    const content = await convertToCsv(cards
+        .map(transformDates)
+        .sort( (a, b) => { return a.id < b.id ? -1 : 1; })
+    );
     const blob = new Blob([content]);
     return {
         content: content,
@@ -11,21 +14,23 @@ export async function getCsvData(cards, boardName) {
     }
 };
 
-function transformDates(card) {
+function transformDates(card: Card) {
     return {
         id: card.id,
         name: card.name,
-        actions: card.actions.map(action => {
-            return {
-                type: action.type,
-                startColumn: action.startColumn,
-                endColumn: action.endColumn,
-                date: action.date.toDate()
-            }
-        })
+        actions: card.actions
+            .map(action => {
+                return {
+                    type: action.type,
+                    startColumn: action.startColumn,
+                    endColumn: action.endColumn,
+                    date: action.date.toDate()
+                }
+            })
+            .sort( (a, b) => { return a.date.getTime() - b.date.getTime(); })
     }
 }
 
 async function convertToCsv(cards) {
-    return await jsonexport(cards);
+    return await jsonexport(cards, {fillGaps: true});
 }
--- test/Filter/Query/csvExporter.test.js
+++ test/Filter/Query/csvExporter.test.js
@@ -2,7 +2,7 @@ import moment from "moment";
 import { getCsvData } from "../../../app/Filter/Query/csvExporter";
 
 describe("Converting CSV", () => {
-    it("Returns the correct data structure for a single card", async () => {
+    it("Returns the correct data structure for a single card with one action", async () => {
 
         global.URL.createObjectURL = jest.fn(() => "myUrl");
 
@@ -27,6 +27,137 @@ describe("Converting CSV", () => {
                 color: 'red'
             }],
             actions: [{
+                type: "updateCard",
+                startColumn: {
+                    id: "0",
+                    name: "ToDo"
+                },
+                endColumn: {
+                    id:"1",
+                    name:"Doing"
+                },
+                date: moment("2020-01-01T16:00:00.000Z"),
+            }]
+        }];
+
+        const actual = await getCsvData(cards, boardName);
+
+        expect(actual).toEqual(expected);
+    });
+    
+    it("Returns the correct data structure for a single card with two actions", async () => {
+
+        global.URL.createObjectURL = jest.fn(() => "myUrl");
+
+        jest.spyOn(global.Date.prototype, 'toISOString')
+            .mockImplementationOnce(() => '2019-05-14');
+
+        const expected = {
+            content: `id,name,actions.type,actions.startColumn.id,actions.startColumn.name,actions.endColumn.id,actions.endColumn.name,actions.date
+1,test,createCard,,,0,ToDo,1/1/2020
+1,test,updateCard,0,ToDo,1,Doing,1/2/2020`,
+            url: "myUrl",
+            filename: "myBoard-2019-05-14.csv"
+        };
+
+        const boardName = "myBoard";
+ 
+        const cards = [{
+            id: 1,
+            name: "test",
+            labels: [{
+                id: '1',
+                name: 'myLabel',
+                color: 'red'
+            }],
+            actions: [{
+                type: "createCard",
+                startColumn: {
+                    id: null,
+                    name: null 
+                },
+                endColumn: {
+                    id:"0",
+                    name:"ToDo"
+                },
+                date: moment("2020-01-01T16:00:00.000Z"),
+            }, {
+                type: "updateCard",
+                startColumn: {
+                    id: "0",
+                    name: "ToDo"
+                },
+                endColumn: {
+                    id:"1",
+                    name:"Doing"
+                },
+                date: moment("2020-01-02T16:00:00.000Z"),
+            }]
+        }];
+
+        const actual = await getCsvData(cards, boardName);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it("Sorts data by card id and date", async () => {
+
+        global.URL.createObjectURL = jest.fn(() => "myUrl");
+
+        jest.spyOn(global.Date.prototype, 'toISOString')
+            .mockImplementationOnce(() => '2019-05-14');
+
+        const expected = {
+            content: `id,name,actions.type,actions.startColumn.id,actions.startColumn.name,actions.endColumn.id,actions.endColumn.name,actions.date
+1,test,createCard,,,0,ToDo,12/31/2019
+1,test,updateCard,0,ToDo,1,Doing,1/1/2020
+2,test2,createCard,,,0,ToDo,11/1/2019`,
+            url: "myUrl",
+            filename: "myBoard-2019-05-14.csv"
+        };
+
+        const boardName = "myBoard";
+ 
+        const cards = [{
+            id: "2",
+            name: "test2",
+            labels: [{
+                id: '1',
+                name: 'myLabel',
+                color: 'red'
+            }],
+            actions: [{
+                type: "createCard",
+                startColumn: {
+                    id: null,
+                    name: null 
+                },
+                endColumn: {
+                    id:"0",
+                    name:"ToDo"
+                },
+                date: moment("2019-11-01T16:00:00.000Z"),
+            }]
+         }, {
+            id: "1",
+            name: "test",
+            labels: [{
+                id: '1',
+                name: 'myLabel',
+                color: 'red'
+            }],
+            actions: [{
+                type: "createCard",
+                startColumn: {
+                    id: null,
+                    name: null 
+                },
+                endColumn: {
+                    id:"0",
+                    name:"ToDo"
+                },
+                date: moment("2019-12-31T16:00:00.000Z"),
+            }, {
                 type: "updateCard",
                 startColumn: {
                     id: "0",
