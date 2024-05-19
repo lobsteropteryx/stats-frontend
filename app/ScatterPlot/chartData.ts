b482e7c2e6327794dee924ecd578b9ff35999944
Ian Firkin <ian.firkin@joinhonor.com>
2024-05-19 09:06:02 -0400

    Removes model code and uses package

--- app/ScatterPlot/chartData.ts
+++
@@ -1,7 +0,0 @@
-export interface ChartData {
-    id: string,
-    name: string,
-    url: string,
-    x: string,
-    y: number
-}
--- app/actionFilter.ts
+++
@@ -1,7 +0,0 @@
-import { Action, Column } from './card';
-
-export function wasMovedBack(action: Action, columns: Column[]): boolean {
-    const startIndex = columns.findIndex(x => x.id === action.startColumn.id);
-    const endIndex = columns.findIndex(x => x.id === action.endColumn.id);
-    return startIndex > endIndex;
-}
