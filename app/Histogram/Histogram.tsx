ba48b9da4efe1727517c9be7a82c9eb9abd67dd4
Ian Firkin <ian.firkin@joinhonor.com>
2024-05-19 10:25:17 -0400

    Converts Histogram module to typescript

--- app/Histogram/Histogram.tsx
+++ app/Histogram/Histogram.tsx
@@ -8,25 +8,14 @@ const Histogram = (props) => (
             indexBy='id'
             groupMode='grouped'
             colors={{ scheme: 'dark2' }}
-            nodeSize={12}
             margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
-            xScale={{
-                type: 'time',
-                format: '%Y-%m-%d',
-                precision: 'day',
-            }}
-            xFormat="time:%Y-%m-%d"
-            yScale={{ type: 'linear', min: 0, max: 'auto' }}
-            yFormat={function(e){return e+" days"}}
             enableGridY={true}
             axisBottom={{
-                orient: 'bottom',
                 legend: 'Days to Complete',
                 legendPosition: 'middle',
                 legendOffset: 60
             }}
             axisLeft={{
-                orient: 'left',
                 legend: 'Completed Stories',
                 legendPosition: 'middle',
                 legendOffset: -60
