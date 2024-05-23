d46318304eb301385ef08795b7b0bece1511d2c0
Ian Firkin <ian.firkin@joinhonor.com>
2024-05-23 19:22:08 -0400

    Replaces jest config file

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  roots: ["test"],
+  extensionsToTreatAsEsm: ['.ts'],
+  verbose: true,
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+  },
+  testPathIgnorePatterns: ['./dist']
+}
+
+export default config
