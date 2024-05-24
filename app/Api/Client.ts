0c807fee28d7b5830562bd1d7ab9424bcf4a3dfd
Ian Firkin <ian.firkin@joinhonor.com>
2024-05-24 02:29:39 -0400

    Deduces base url for api calls

--- app/Api/Client.ts
+++ app/Api/Client.ts
@@ -1,60 +1,36 @@
 import axios, { AxiosInstance } from 'axios';
 import { Board, Card, Label, Column } from '@lobsteropteryx/stats-models';
 
-const API_LIMIT = 250;
-
 export class Client {
-    key:string;
-    token:string;
-    axiosInstance:AxiosInstance;
+    #axiosInstance:AxiosInstance;
     
-    constructor(baseUrl) {
-        this.axiosInstance = axios.create({
+    constructor(baseUrl = `${window.location.host}/api/`) {
+        this.#axiosInstance = axios.create({
             baseURL: baseUrl
         });
     }
 
     getBoards():Promise<Board[]> {
-        return this.axiosInstance
-            .get(`/boards`, {
-                params: {
-                    key: this.key,
-                    token: this.token
-                }
-            })
+        return this.#axiosInstance
+            .get(`/boards`, {})
             .then(response => response.data);
     }
     
     getColumnsForBoard(boardId:string):Promise<Column[]> {
-        return this.axiosInstance
-            .get(`/boards/${boardId}/columns`, { 
-                params: {
-                    key: this.key,
-                    token: this.token
-                } 
-            })
+        return this.#axiosInstance
+            .get(`/boards/${boardId}/columns`, {})
             .then(response => response.data);
     }
 
     getLabelsForBoard(boardId:string):Promise<Label[]> {
-        return this.axiosInstance
-            .get(`/boards/${boardId}/labels`, { 
-                params: {
-                    key: this.key,
-                    token: this.token
-                } 
-            })
+        return this.#axiosInstance
+            .get(`/boards/${boardId}/labels`, {})
             .then(response => response.data);
     }
     
     getCardsForBoard(boardId:string):Promise<Card[]> {
-        return this.axiosInstance
-            .get(`/boards/${boardId}/cards`, { 
-                params: {
-                    key: this.key,
-                    token: this.token
-                } 
-            })
+        return this.#axiosInstance
+            .get(`/boards/${boardId}/cards`, {})
             .then(response => response.data);
     }
 }
